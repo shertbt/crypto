3daf5d14c4d792b45c03df87ab63440943e4e9ad
shertbt <61592718+shertbt@users.noreply.github.com>
2020-09-08 15:02:49 +0300

    Update 6.2.cpp

--- 6.2.cpp
+++ 6.2.cpp
@@ -4,7 +4,7 @@ int main()
 {
     Crypto crypt;
 
-    //The test cases provided
+    
     string str1 = "1c0111001f010100061a024b53535009181c";
     string str2 = "686974207468652062756c6c277320657965";
 
