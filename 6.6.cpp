fe03ad78c84b65be9e9a056bfb866d031e3e7692
shertbt <61592718+shertbt@users.noreply.github.com>
2020-09-08 15:04:18 +0300

    Update 6.6.cpp

--- 6.6.cpp
+++ 6.6.cpp
@@ -10,17 +10,17 @@ int main()
 	ifstream infile;
 
 
-	infile.open("C:\\Users\\Äìèòðèé\\Desktop\\text6.6.txt");
+	infile.open("C:\\Users\\Ã„Ã¬Ã¨Ã²Ã°Ã¨Ã©\\Desktop\\text6.6.txt");
 	getline(infile, message, '\0');
 	infile.close();
 
-	//Convert the hexadecimal string to ASCII
+	
 	ascii_mess = crypt.con_hex_2_ascii(crypt.con_b64_2_hex(message));
 
-	//Guess the length of the key
+	
 	int keyLength = crypt.guess_key_length(ascii_mess);
 
-	//Transpose the message based on keysize length
+	
 	int blocks = ascii_mess.size() / keyLength;
 
 	for (int i = 0; i < keyLength; ++i)
@@ -28,16 +28,12 @@ int main()
 		string block;
 		char indexKey = '\0';
 
-		/*
-			Transpose the message into blocks
-			based on keysize and concatenate it
-			into one string
-		*/
+		
 		for (int j = 0; j < blocks; j++) {
 			block += ascii_mess.substr((j * keyLength) + i, 1);
 		}
 
-		//Concatenate the selected characters, which will become the predicted key
+		
 		key += crypt.single_byte_xor(block);
 	}
 
