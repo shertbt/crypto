c0717bdb767573d9aa0271c6662a1891d4375e1b
shertbt <61592718+shertbt@users.noreply.github.com>
2020-09-08 14:46:45 +0300

    Add files via upload

---
+++ 6.8.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <math.h>
+#include <map>
+
+using namespace std;
+
+
+string detectAESinECB(string file) {
+    ifstream input_file;
+    input_file.open(file);
+    string str;
+    int maxcounter = 0;
+    string result;
+    while (!input_file.eof())
+    {
+        getline(input_file, str);
+        int block_size = 16;
+
+        int blocks = str.size() / block_size;
+        int count = 0;
+        for (int i = 0; i < blocks; i++)
+        {
+            
+            string strA = str.substr(i * block_size, block_size);
+
+            for (int j = i + 1; j < blocks; j++)
+            {
+                
+                string strB = str.substr(j * block_size, block_size);
+                if (strA == strB)
+                {
+                    count++;
+
+                }
+            }
+        }
+        if (maxcounter < count)
+        {
+            maxcounter = count;
+            result = str;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    cout << detectAESinECB("C:\\Users\\ִלטענטי\\Desktop\\text6.8.txt") << endl;
+    return 0;
+}
